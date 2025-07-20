978411ffc382606fbb5b8e7ba3fafe71e352cc6e
dotnetprog <24593889+dotnetprog@users.noreply.github.com>
2025-07-20 15:22:04 -0400

    Removal of Cocona and unit tests for new desing

---
+++ src/Dataverse.ConfigurationMigrationTool/Console.Tests/Features/CommandProcessorHostingServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using Dataverse.ConfigurationMigrationTool.Console.Features;
+using Dataverse.ConfigurationMigrationTool.Console.Features.Shared;
+using Dataverse.ConfigurationMigrationTool.Console.Tests.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using Shouldly;
+using System.Reflection;
+
+namespace Dataverse.ConfigurationMigrationTool.Console.Tests.Features;
+public class CommandProcessorHostingServiceTests
+{
+    private readonly IOptions<CommandProcessorHostingServiceOptions> _options;
+    private readonly IServiceScopeFactory _serviceProviderFactory;
+    private readonly IHostApplicationLifetime _lifetime;
+    private readonly IServiceScope _serviceScope;
+    private readonly IServiceCollection ServiceCollection = new ServiceCollection();
+    private readonly ILogger<FakeCommand> commandLogger = Substitute.For<ILogger<FakeCommand>>();
+    private readonly CommandProcessorHostingServiceOptions HostingOptions = new CommandProcessorHostingServiceOptions
+    {
+        CommandVerb = "fake"
+    };
+
+    public CommandProcessorHostingServiceTests()
+    {
+
+        ServiceCollection.AddSingleton<ILogger<FakeCommand>>(commandLogger);
+        _serviceScope = Substitute.For<IServiceScope>();
+        _serviceScope.ServiceProvider.Returns(ServiceCollection.BuildServiceProvider());
+        _options = Substitute.For<IOptions<CommandProcessorHostingServiceOptions>>();
+        _options.Value.Returns(HostingOptions);
+        _serviceProviderFactory = Substitute.For<IServiceScopeFactory>();
+        _serviceProviderFactory.CreateScope().Returns(_serviceScope);
+        _lifetime = Substitute.For<IHostApplicationLifetime>();
+
+    }
+
+    [Fact]
+    public async Task GivenACommandVerb_WhenCommandProcessorRuns_ThenItShouldExecuteTheProperCommand()
+    {
+        //Arrange
+
+        var host = new CommandProcessorHostingService(_options, _serviceProviderFactory, _lifetime, Assembly.GetExecutingAssembly());
+        //Act
+        await host.StartAsync(CancellationToken.None);
+
+        //Assert
+        commandLogger.ShouldHaveLogged(LogLevel.Information, "Fake command executed successfully.");
+
+    }
+
+    [Fact]
+    public async Task GivenAnNonExistantCommandVerb_WhenCommandProcessorRuns_ThenItShouldExecuteTheProperCommand()
+    {
+        //Arrange
+        var host = new CommandProcessorHostingService(_options, _serviceProviderFactory, _lifetime);
+        //Act
+        Func<Task> act = () => host.StartAsync(CancellationToken.None);
+
+        //Assert
+        var ex = await act.ShouldThrowAsync<InvalidOperationException>();
+        ex.Message.ShouldBe($"No command found for verb '{HostingOptions.CommandVerb}'");
+
+    }
+
+
+
+}
+[CommandVerb("fake")]
+public class FakeCommand : ICommand
+{
+    private readonly ILogger<FakeCommand> _logger;
+
+    public FakeCommand(ILogger<FakeCommand> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Execute()
+    {
+        _logger.LogInformation("Fake command executed successfully.");
+        return Task.CompletedTask;
+    }
+}
--- src/Dataverse.ConfigurationMigrationTool/Dataverse.ConfigurationMigrationTool.Console/Program.cs
+++ src/Dataverse.ConfigurationMigrationTool/Dataverse.ConfigurationMigrationTool.Console/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.PowerPlatform.Dataverse.Client;
 using System.Reflection;
-var builder = new HostBuilder();
+var builder = Host.CreateDefaultBuilder(args);
 builder.ConfigureHostConfiguration((config) =>
 {
     // Configure the host configuration, such as environment variables, command line arguments, etc.
@@ -33,10 +33,6 @@ builder.ConfigureAppConfiguration((context, config) =>
         config.AddUserSecrets(Assembly.GetExecutingAssembly());
     }
     Console.WriteLine($"Using configuration file: appsettings.{context.HostingEnvironment.EnvironmentName}.json");
-    foreach (var arg in context.Configuration.AsEnumerable())
-    {
-        Console.WriteLine($"Configuration: {arg.Key} => {arg.Value}");
-    }
 });
 builder.ConfigureServices((context, services) =>
 {
@@ -56,44 +52,6 @@ builder.ConfigureServices((context, services) =>
     .AddImportFeature(context.Configuration);
     // Configure other services.
 });
-builder.ConfigureCocona(args, configureApplication: app =>
-{
-    // Configure your app's commands normally as you would with app
-    app.UseImportFeature();
-});
+
 var app = builder.Build();
 await app.RunAsync();
-//var builder = CoconaApp.CreateBuilder();
-//builder.Configuration
-//        .AddEnvironmentVariables()
-//        .AddJsonFile("appsettings.json", false, false)
-//        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", false, false);
-//Console.WriteLine($"Using configuration file: appsettings.{builder.Environment.EnvironmentName}.json");
-//foreach (var arg in args)
-//{
-//    Console.WriteLine($"Argument: {arg}");
-//}
-//if (!builder.Environment.IsProduction())
-//{
-//    //Secrets should never be in clear text in source controlled file such appsettings.json.
-//    //For Developement, we therefore store them locally into UserSecrets Store, part of dotnet foundation.
-//    //For Production, secrets can be either written into appsettings.Production.json file by pipeline
-//    //or you can configure another Configuration Provider to provide the secrets like AzureKeyvault or Hashicorp Vault.
-//    builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly());
-//}
-//builder.Services
-//    .AddLogging(lb => lb.AddConsole())
-//    .Configure<SdkDataverseServiceFactoryOptions>(builder.Configuration.GetSection("Dataverse"))
-//    .Configure<ParallelismBulkOrganizationServiceOptions>(builder.Configuration.GetSection("Dataverse"))
-//    .AddTransient<IImportDataProvider, FileReaderDataImportProvider>()
-//    .AddSingleton<IFileDataReader, XmlFileDataReader>()
-//    .AddTransient<IMetadataService, DataverseMetadataService>()
-//    .AddTransient<ServiceClient>((sp) => (ServiceClient)sp.GetRequiredService<IDataverseClientFactory>().Create())
-//    .AddSingleton<IBulkOrganizationService, ParallelismBulkOrganizationService>()
-//    .AddDataverseClient()
-//    .AddImportFeature();
-//Console.WriteLine($"Services are completed");
-//var app = builder.Build();
-//app.UseImportFeature();
-//Console.WriteLine($"Running App");
-//await app.RunAsync();
